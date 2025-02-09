205f7b2fdbcc717769ad6380b1136c464e96a4c8
Денис Ионов <di1977@ya.ru>
2025-02-09 11:09:33 +0300

    - red colors

---
+++ bright/userConfig.lua
@@ -0,0 +1,30 @@
+TZHour = 3
+TZMin = 0
+BGHour = { 1,0,0 }
+BGMin = { 1,0,0 }
+BGSec = { 1,0,0 }
+FGHour = { 7,4,1 }
+FGMin = { 7,4,1 }
+FGSec = { 7,4,1 }
+
+dBGHour = { 200,0,0 }
+dBGMin = { 200,0,0 }
+dBGSec = { 200,0,0 }
+dFGHour = { 255,255,255 }
+dFGMin = { 255,255,255 }
+dFGSec = { 255,255,255 }
+
+Day = 7
+Night = 21
+
+topToBottom = false
+
+resyncH = 4
+resyncM = 0
+resyncS = 0
+
+autoTZ = true
+httpFallback = true
+
+displayDate = false
+displayWeekday = true
